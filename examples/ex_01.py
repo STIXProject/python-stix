e3b8c2b2ce22bdfdcdee58c229b022271e7aeea7
Bryan Worrell <bworrell@mitre.org>
2013-03-13 19:39:17 -0400

    fixed example ex_01

--- examples/ex_01.py
+++ examples/ex_01.py
@@ -7,14 +7,14 @@ from stix.indicator import Indicator
 import stix.bindings.stix_core_1_0 as stix_core_binding
 
 def main():
-    fn = 'email_indicator_instance.xml'
-    stix_package_obj = stix_core_binding.parse(fn) # create a binding object
-    stix_package = STIXPackage.from_obj(stix_package_obj)
-    stix_dict = stix_package.to_dict()
+    fn = 'ex_01.xml'
+    stix_package_obj = stix_core_binding.parse(fn) # create a binding object from xml
+    stix_package = STIXPackage.from_obj(stix_package_obj) # create python-stix object from binding
+    stix_dict = stix_package.to_dict() # parse to dictionary
     pprint(stix_dict)
-    #other way
-    stix_package_two = STIXPackage.from_dict(stix_dict)
-    xml = stix_package_two.to_xml()
+    
+    stix_package_two = STIXPackage.from_dict(stix_dict) # create python-stix object from dictionary
+    xml = stix_package_two.to_xml() # generate xml from python-stix object
     print xml
     
 if __name__ == '__main__':
