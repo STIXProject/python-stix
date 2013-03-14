127216527a71ce3afea147cfcb4cfa69efc645ee
Bryan Worrell <bworrell@mitre.org>
2013-03-14 15:06:38 -0400

    added the beginning of an ex_02.py script

---
+++ examples/ex_02.py
@@ -0,0 +1,17 @@
+'''
+File: ex_02.py
+
+Description: Build a STIX Indicator document containing a File observable with an associated hash.
+'''
+
+from stix.indicator import Indicator
+from stix.core import STIXPackage
+from cybox.core import Observable
+import cybox.objects.file_object.File as File
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
