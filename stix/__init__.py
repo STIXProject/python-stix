2e0686ea19a6a60f9056e800c05e4ab97b9f98d2
Bryan Worrell <bworrell@mitre.org>
2013-06-27 10:34:43 -0400

    bumping version

--- stix/__init__.py
+++ stix/__init__.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2013, The MITRE Corporation. All rights reserved.
 # See LICENSE.txt for complete terms.
 
-__version__ = "1.0.0a4"
+__version__ = "1.0.0a5"
 
 import json
 from StringIO import StringIO
