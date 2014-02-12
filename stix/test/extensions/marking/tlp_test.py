2746609faddcd5a1f2c00e3adf3ceda2c6aa2745
Greg Back <gback@mitre.org>
2014-02-12 11:53:58 -0500

    Update Marking classes and implement TLP extension

---
+++ stix/test/extensions/marking/tlp_test.py
@@ -0,0 +1,20 @@
+import unittest
+
+from cybox.test import EntityTestCase
+
+from stix.extensions.marking.tlp import TLPMarkingStructure
+
+
+class TLPMarkingStructureTests(EntityTestCase, unittest.TestCase):
+    klass = TLPMarkingStructure
+    _full_dict = {
+        'marking_model_name': 'TLP',
+        'marking_model_ref': "http://www.us-cert.gov/tlp/",
+        'color': "RED",
+        'xsi:type': "tlpMarking:TLPMarkingStructureType",
+    }
+
+
+if __name__ == "__main__":
+    unittest.main()
+
