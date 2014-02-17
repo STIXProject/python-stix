f8112469e5f44a9b2ced7f78d507254dee714a24
Greg Back <gback@mitre.org>
2014-02-17 11:07:07 -0500

    Clean up common imports

--- stix/common/__init__.py
+++ stix/common/__init__.py
@@ -1,11 +1,14 @@
 # Copyright (c) 2014, The MITRE Corporation. All rights reserved.
 # See LICENSE.txt for complete terms.
 
-from identity import *
-from information_source import InformationSource
-from generic_relationship import GenericRelationship
-from structured_text import StructuredText
-from vocabs import VocabString
+from __future__ import absolute_import
+
+from .identity import *
+from .information_source import InformationSource
+from .generic_relationship import GenericRelationship
+from .structured_text import StructuredText
+from .vocabs import VocabString
+
 
 class HighMediumLow(VocabString):
     _namespace = 'http://stix.mitre.org/default_vocabularies-1'
