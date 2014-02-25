e4496929b30b01236b49ec9128e2708d4eec34dd
Bryan Worrell <bworrell@mitre.org>
2014-02-25 10:12:03 -0500

    Bumped version to 1.1.0.2

--- stix/__init__.py
+++ stix/__init__.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2014, The MITRE Corporation. All rights reserved.
 # See LICENSE.txt for complete terms.
 
-__version__ = "1.1.0.1"
+__version__ = "1.1.0.2"
 
 import json
 from StringIO import StringIO
