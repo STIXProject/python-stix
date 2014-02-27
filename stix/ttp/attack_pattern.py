60aaafd9c95c89e688527c3dcf0884052ddd0705
Bryan Worrell <bworrell@mitre.org>
2014-02-27 10:39:06 -0500

    Implemented AttackPattern, Exploit, and MalwareInstance

--- stix/ttp/attack_pattern.py
+++ stix/ttp/attack_pattern.py
@@ -2,6 +2,8 @@
 # See LICENSE.txt for complete terms.
 
 import stix
+import stix.utils
+from stix.common import StructuredText
 import stix.bindings.ttp as ttp_binding
 
 class AttackPattern(stix.Entity):
@@ -9,16 +11,105 @@ class AttackPattern(stix.Entity):
     _binding_class = _binding.AttackPatternType
     _namespace = "http://stix.mitre.org/TTP-1"
     
+    def __init__(self, id_=None, title=None, description=None, short_description=None):
+        self.id_ = id_ or stix.utils.create_id("attackpattern")
+        self.capec_id = None
+        self.title = title
+        self.description = description
+        self.short_description = short_description
+        
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        self._title = value
+
+    @property
+    def description(self):
+        return self._description
+
+    @description.setter
+    def description(self, value):
+        if value:
+            if isinstance(value, StructuredText):
+                self._description = value
+            else:
+                self._description = StructuredText(value=value)
+        else:
+            self._description = None
+
+    @property
+    def short_description(self):
+        return self._short_description
+
+    @short_description.setter
+    def short_description(self, value):
+        if value:
+            if isinstance(value, StructuredText):
+                self._short_description = value
+            else:
+                self._short_description = StructuredText(value=value)
+        else:
+            self._short_description = None
+
     def to_obj(self, return_obj=None):
-        return None
-    
+        if not return_obj:
+            return_obj = self._binding_class()
+
+        return_obj.set_id(self.id_)
+        return_obj.set_capec_id(self.capec_id)
+        return_obj.set_Title(self.title)
+
+        if self.description:
+            return_obj.set_Description(self.description.to_obj())
+        if self.short_description:
+            return_obj.set_Short_Description(self.short_description.to_obj())
+
+        return return_obj
+
     @classmethod
     def from_obj(cls, obj, return_obj=None):
-        return None
-    
+        if not obj:
+            return None
+        if not return_obj:
+            return_obj = cls()
+
+        return_obj.id_ = obj.get_id()
+        return_obj.capec_id = obj.get_capec_id()
+        return_obj.title = obj.get_Title()
+        return_obj.description = StructuredText.from_obj(obj.get_Description())
+        return_obj.short_description = StructuredText.from_obj(obj.get_Short_Description())
+        
+        return return_obj
+
     def to_dict(self):
-        return {}
-    
+        d = {}
+        if self.id_:
+            d['id'] = self.id_
+        if self.capec_id:
+            d['capec_id'] = self.capec_id
+        if self.title:
+            d['title'] = self.title
+        if self.description:
+            d['description'] = self.description.to_dict()
+        if self.short_description:
+            d['short_description'] = self.short_description.to_dict()
+       
+        return d
+
     @classmethod
     def from_dict(cls, dict_repr, return_obj=None):
-        return None
+        if not dict_repr:
+            return None
+        if not return_obj:
+            return_obj = cls()
+
+        return_obj.id_ = dict_repr.get('id')
+        return_obj.capec_id = dict_repr.get('capec_id')
+        return_obj.title = dict_repr.get('title')
+        return_obj.description = StructuredText.from_dict(dict_repr.get('description'))
+        return_obj.short_description = StructuredText.from_dict(dict_repr.get('short_description'))
+        
+        return return_obj
