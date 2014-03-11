34d46103a3b2074142d3d8b4eee4face88268709
Greg Back <gback@mitre.org>
2014-03-11 12:59:34 -0400

    Simplify GenericRelationshipLists

--- stix/incident/attributed_threat_actors.py
+++ stix/incident/attributed_threat_actors.py
@@ -1,83 +1,20 @@
 # Copyright (c) 2014, The MITRE Corporation. All rights reserved.
 # See LICENSE.txt for complete terms.
 
-import stix
 import stix.bindings.incident as incident_binding
 from stix.common.generic_relationship import GenericRelationshipList
-from stix.threat_actor import ThreatActor
 from stix.common.related import RelatedThreatActor
+
 
 class AttributedThreatActors(GenericRelationshipList):
     _namespace = "http://stix.mitre.org/Incident-1"
     _binding = incident_binding
     _binding_class = incident_binding.AttributedThreatActorsType
+    _binding_var = "Threat_Actor"
+    _contained_type = RelatedThreatActor
+    _inner_name = "threat_actors"
 
     def __init__(self, threat_actors=None, scope=None):
-        super(AttributedThreatActors, self).__init__(scope=scope)
-        self.threat_actors = threat_actors
-
-    @property
-    def threat_actors(self):
-        return self._threat_actors
-
-    @threat_actors.setter
-    def threat_actors(self, value):
-        self._threat_actors = []
-
-        if isinstance(value, list):
-            for v in value:
-                self.add_threat_actor(v)
-        else:
-            self.add_threat_actor(value)
-
-    def add_threat_actor(self, threat_actor):
-        if not threat_actor:
-            return
-        elif isinstance(threat_actor, RelatedThreatActor):
-            self.threat_actors.append(threat_actor)
-        elif isinstance(threat_actor, ThreatActor):
-            self.threat_actors.append(RelatedThreatActor(threat_actor=threat_actor))
-        else:
-            raise ValueError('value must be instance of RelatedThreatActor')
-
-    def to_obj(self, return_obj=None):
-        if not return_obj:
-            return_obj = self._binding_class()
-
-        super(AttributedThreatActors, self).to_obj(return_obj)
-        return_obj.set_Threat_Actor([x.to_obj() for x in self.threat_actors])
-        return return_obj
-
-    @classmethod
-    def from_obj(cls, obj, return_obj=None):
-        if not obj:
-            return None
-
-        if not return_obj:
-            return_obj = cls()
-
-        super(AttributedThreatActors, cls).from_obj(obj, return_obj)
-
-        if obj.get_Threat_Actor():
-            return_obj.threat_actors = [RelatedThreatActor.from_obj(x) for x in obj.get_Threat_Actor()]
-
-        return return_obj
-
-    def to_dict(self):
-        d = super(AttributedThreatActors, self).to_dict()
-        if self.threat_actors:
-            d['threat_actors'] = [x.to_dict() for x in self.threat_actors]
-
-        return d
-
-    @classmethod
-    def from_dict(cls, dict_repr, return_obj=None):
-        if not dict_repr:
-            return None
-
-        if not return_obj:
-            return_obj = cls()
-
-        super(AttributedThreatActors, cls).from_dict(dict_repr, return_obj)
-        return_obj.threat_actors = [RelatedThreatActor.from_dict(x) for x in dict_repr.get('threat_actors', [])]
-        return return_obj
+        if threat_actors is None:
+            threat_actors = []
+        super(AttributedThreatActors, self).__init__(*threat_actors, scope=scope)
--- stix/incident/related_indicators.py
+++ stix/incident/related_indicators.py
@@ -1,84 +1,20 @@
 # Copyright (c) 2014, The MITRE Corporation. All rights reserved.
 # See LICENSE.txt for complete terms.
 
-import stix
 import stix.bindings.incident as incident_binding
 from stix.common.generic_relationship import GenericRelationshipList
 from stix.common.related import RelatedIndicator
-from stix.indicator import Indicator
-from cybox.core import Observable, Object
+
 
 class RelatedIndicators(GenericRelationshipList):
     _namespace = "http://stix.mitre.org/Incident-1"
     _binding = incident_binding
     _binding_class = incident_binding.RelatedIndicatorsType
+    _binding_var = "Related_Indicator"
+    _contained_type = RelatedIndicator
+    _inner_name = "indicators"
 
     def __init__(self, indicators=None, scope=None):
-        super(RelatedIndicators, self).__init__(scope=scope)
-        self.indicators = indicators
-
-    @property
-    def indicators(self):
-        return self._indicators
-
-    @indicators.setter
-    def indicators(self, value):
-        self._indicators = []
-
-        if isinstance(value, list):
-            for v in value:
-                self.add_indicator(v)
-        else:
-            self.add_indicator(value)
-
-    def add_indicator(self, indicator):
-        if not indicator:
-            return
-        if isinstance(indicator, RelatedIndicator):
-            self.indicators.append(indicator)
-        elif isinstance(indicator, Indicator):
-            self.indicators.append(RelatedIndicator(indicator=indicator))
-        else:
-            raise ValueError("Cannot add %s to indicators list" % type(indicator))
-
-    def to_obj(self, return_obj=None):
-        if not return_obj:
-            return_obj = self._binding_class()
-
-        super(RelatedIndicators, self).to_obj(return_obj)
-        return_obj.set_Related_Indicator([x.to_obj() for x in self.indicators])
-        return return_obj
-
-    @classmethod
-    def from_obj(cls, obj, return_obj=None):
-        if not obj:
-            return None
-
-        if not return_obj:
-            return_obj = cls()
-
-        super(RelatedIndicators, cls).from_obj(obj, return_obj)
-
-        if obj.get_Related_Indicator():
-            return_obj.indicators = [RelatedIndicator.from_obj(x) for x in obj.get_Related_Indicator()]
-
-        return return_obj
-
-    def to_dict(self):
-        d = super(RelatedIndicators, self).to_dict()
-        if self.indicators:
-            d['indicators'] = [x.to_dict() for x in self.indicators]
-
-        return d
-
-    @classmethod
-    def from_dict(cls, dict_repr, return_obj=None):
-        if not dict_repr:
-            return None
-
-        if not return_obj:
-            return_obj = cls()
-
-        super(RelatedIndicators, cls).from_dict(dict_repr, return_obj)
-        return_obj.indicators = [RelatedIndicator.from_dict(x) for x in dict_repr.get('indicators', [])]
-        return return_obj
+        if indicators is None:
+            indicators = []
+        super(RelatedIndicators, self).__init__(*indicators, scope=scope)
