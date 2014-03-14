1f059c99381440115a5998e01cd70b6ab63effa5
Greg Back <gback@mitre.org>
2014-03-14 14:32:08 -0400

    Don't import package to determine version.

--- setup.py
+++ setup.py
@@ -1,18 +1,28 @@
 # Copyright (c) 2014, The MITRE Corporation. All rights reserved.
 # See LICENSE.txt for complete terms.
 
+from os.path import abspath, dirname, join
 from setuptools import setup, find_packages
-import stix
+
+INIT_FILE = join(dirname(abspath(__file__)), 'stix', '__init__.py')
+
+def get_version():
+    with open(INIT_FILE) as f:
+        for line in f.readlines():
+            if line.startswith("__version__"):
+                version = line.split()[-1]
+                return version
+        raise AttributeError("Package does not have a __version__")
 
 setup(
     name="stix",
-    version=stix.__version__,
+    version=get_version(),
     author="STIX Project, MITRE Corporation",
     author_email="stix@mitre.org",
     description="An API for parsing and generating STIX content.",
     url="http://stix.mitre.org",
     packages=find_packages(),
-    install_requires=['lxml>=2.3', 'python-dateutil', 'cybox>=2.1.0.1,<2.1.1.0' ],
+    install_requires=['lxml>=2.3', 'python-dateutil', 'cybox>=2.1.0.1,<2.1.1.0'],
     classifiers=[
         "Programming Language :: Python",
         "Development Status :: 2 - Pre-Alpha",
