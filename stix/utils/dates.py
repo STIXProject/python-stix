a5767582cdf38f2de12eb06b83f8b28a4357e1e9
Bryan Worrell <bworrell@mitre.org>
2014-03-19 13:03:21 -0400

    Added missing stix.utils.dates

---
+++ stix/utils/dates.py
@@ -0,0 +1,17 @@
+# Copyright (c) 2014, The MITRE Corporation. All rights reserved.
+# See LICENSE.txt for complete terms.
+
+import dateutil
+from datetime import datetime
+
+def parse_value(value):
+    if not value:
+        return None
+    elif isinstance(value, datetime):
+        return value
+    return dateutil.parser.parse(value)
+
+def serialize_value(value):
+    if not value:
+        return None
+    return value.isoformat()
