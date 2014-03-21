19e6fb09de69c957a4b2a127a5aec300d3ac7be1
Bryan Worrell <bworrell@mitre.org>
2014-03-21 10:43:03 -0400

    Initial, untested, commit of affecte_asset and property_affected modules.

---
+++ stix/incident/affected_asset.py
@@ -0,0 +1,285 @@
+# Copyright (c) 2014, The MITRE Corporation. All rights reserved.
+# See LICENSE.txt for complete terms.
+
+
+import stix
+from stix.common import VocabString, StructuredText
+import stix.bindings.incident as incident_binding
+from .property_affected import PropertyAffected
+from cybox.core import Observables
+
+class AffectedAsset(stix.Entity):
+    _namespace = "http://stix.mitre.org/Incident-1"
+    _binding = incident_binding
+    _binding_class = incident_binding.AffectedAssetType
+    
+    def __init__(self):
+        self.type_ = None
+        self.description = None
+        self.business_function_or_role = None
+        self.ownership_class = None
+        self.management_class = None
+        self.location_class = None
+        #self.location = None
+        self.nature_of_security_effect = None
+        self.structured_description =  None
+        
+    @property
+    def type_(self):
+        return self._type
+    
+    @type_.setter
+    def type_(self, value):
+        if not value:
+            self._type = None
+        elif isinstance(value, AssetType):
+            self._type = value
+        else:
+            self._type = AssetType(value=value)
+    
+    @property
+    def description(self):
+        return self._description
+    
+    @description.setter
+    def description(self, value):
+        if not value:
+            self._description = None
+        elif isinstance(value, StructuredText):
+            self._description = value
+        else:
+            self._description = StructuredText(value)
+    
+    @property
+    def business_function_or_role(self):
+        return self._business_function_or_role
+    
+    @business_function_or_role.setter
+    def business_function_or_role(self, value):
+        if not value:
+            self._business_function_or_role = None
+        elif isinstance(value, StructuredText):
+            self._business_function_or_role = value
+        else:
+            self._business_function_or_role = StructuredText(value)
+            
+    @property
+    def ownership_class(self):
+        return self._ownership_class
+    
+    @ownership_class.setter
+    def ownership_class(self, value):
+        if not value:
+            self._ownership_class = None
+        elif isinstance(value, OwnershipClass):
+            self._ownership_class = value
+        else:
+            self._ownership_class = OwnershipClass(value)
+    
+    @property
+    def management_class(self):
+        return self._management_class
+    
+    @management_class.setter
+    def management_class(self, value):
+        if not value:
+            self._management_class = None
+        elif isinstance(value, ManagementClass):
+            self._management_class = value
+        else:
+            self._management_class = ManagementClass(value)
+    
+    @property
+    def location_class(self):
+        return self._location_class
+    
+    @location_class.setter
+    def location_class(self, value):
+        if not value:
+            self._location_class = None
+        elif isinstance(value, LocationClass):
+            self._location_class = value
+        else:
+            self._location_class = LocationClass(value)
+    
+    @property
+    def nature_of_security_effect(self):
+        return self._nature_of_security_effect
+    
+    @nature_of_security_effect.setter
+    def nature_of_security_effect(self, value):
+        self._nature_of_security_effect = []
+        if not value:
+            return
+        elif isinstance(value, list):
+            for v in value:
+                self.add_property_affected(v)
+        else:
+            self.add_property_affected(value)
+            
+    def add_property_affected(self, v):
+        if not v:
+            return
+        elif isinstance(v, PropertyAffected):
+            self.nature_of_security_effect.append(v)
+        else:
+            raise ValueError('Cannot add type %s to nature_of_security_effect list' % type(v))
+    
+    @property
+    def structured_description(self):
+        return self._structured_description
+    
+    @structured_description.setter
+    def structured_description(self, value):
+        if not value:
+            self._structured_description = None
+        elif isinstance(value, Observables):
+            self._structured_description = value
+        else:
+            self._structured_description = Observables(value)
+    
+    @classmethod
+    def from_obj(cls, obj, return_obj=None):
+        if not obj:
+            return None
+        if not return_obj:
+            return_obj = cls()
+            
+        return_obj.type_ = AssetType.from_obj(obj.get_Type())
+        return_obj.description = StructuredText.from_obj(obj.get_Description())
+        return_obj.business_function_or_role = StructuredText.from_obj(obj.get_Business_Role_Or_Function())
+        return_obj.owernship_class = OwnershipClass.from_obj(obj.get_Ownership_Class())
+        return_obj.management_class = ManagementClass.from_obj(obj.get_Management_Class())
+        return_obj.location_class = LocationClass.from_obj(obj.get_Location_Class())
+        #return_obj.location = None 
+        
+        if obj.get_Nature_Of_Security_Effect():
+            n = obj.get_Nature_Of_Security_Effect()
+            return_obj.nature_of_security_effect = [PropertyAffected.from_obj(x) for x in n.get_Property_Affected()]
+        return return_obj
+    
+    def to_obj(self, return_obj=None):
+        if not return_obj:
+            return_obj = self._binding_class()
+        
+        if self.type_:
+            return_obj.set_Type(self.type_.to_obj())
+        if self.description:
+            return_obj.set_Description(self.description.to_obj())
+        if self.business_function_or_role:
+            return_obj.set_Business_Function_Or_Role(self.business_function_or_role.to_obj())
+        if self.ownership_class:
+            return_obj.set_Ownership_Class(self.ownership_class.to_obj())
+        if self.management_class:
+            return_obj.set_Management_Class(self.management_class.to_obj())
+        if self.location_class:
+            return_obj.set_Location_Class(self.location_class.to_obj())
+#         if self.location:
+#             return_obj.set_Location(self.location.to_obj())
+        if self.nature_of_security_effect:
+            property_affected_list = [x.to_obj() for x in self.nature_of_security_effect]
+            n = self._binding.NatureOfSecurityEffectType(Property_Affected=property_affected_list)
+            return_obj.set_Nature_Of_Security_Effect(n)
+        if self.structured_description:
+            return_obj.set_Structured_Description(self.structured_description.to_obj())    
+       
+        return return_obj
+    
+    @classmethod
+    def from_dict(cls, d, return_obj=None):
+        if not d:
+            return None
+        if not return_obj:
+            return_obj = cls()
+        
+        return_obj.type_ = AssetType.from_dict(d.get('type'))
+        return_obj.description = StructuredText.from_dict(d.get('description'))
+        return_obj.business_function_or_role = StructuredText.from_dict(d.get('business_function_or_role'))
+        return_obj.ownership_class = OwnershipClass.from_dict(d.get('ownership_class'))
+        return_obj.management_class = ManagementClass.from_dict(d.get('management_class'))
+        return_obj.location_class = LocationClass.from_dict(d.get('location_class'))
+        #return_obj.location = Location.from_dict(d.get('location'))
+        return_obj.nature_of_security_effect = [PropertyAffected.from_dict(x) for x in d.get('nature_of_security_effect')]
+        return_obj.structured_description = Observables.from_dict(d.get('structured_description'))
+        return return_obj
+    
+    def to_dict(self):
+        d = {}
+        if self.type_:
+            d['type'] = self.type_.to_dict()
+        if self.description:
+            d['description'] = self.description.to_dict()
+        if self.business_function_or_role:
+            d['business_function_or_role'] = self.business_function_or_role.to_dict()
+        if self.ownership_class:
+            d['ownership_class'] = self.ownership_class.to_dict()
+        if self.management_class:
+            d['management_class'] = self.management_class.to_dict()
+        if self.location_class:
+            d['location_class'] = self.location_class.to_dict()
+#         if self.location:
+#             d['location'] = self.location.to_dict()
+        if self.nature_of_security_effect:
+            d['nature_of_security_effect'] = [x.to_dict() for x in self.nature_of_security_effect]
+        if self.structured_description:
+            d['structured_description'] = self.structured_description.to_dict()
+        return d
+    
+class OwnershipClass(VocabString):
+    _namespace = "http://stix.mitre.org/default_vocabularies-1"
+    _XSI_TYPE = "stixVocabs:OwnershipClassVocab-1.0"
+
+class ManagementClass(VocabString):
+    _namespace = "http://stix.mitre.org/default_vocabularies-1"
+    _XSI_TYPE = "stixVocabs:ManagementClassVocab-1.0"
+
+class LocationClass(VocabString):
+    _namespace = "http://stix.mitre.org/default_vocabularies-1"
+    _XSI_TYPE = "stixVocabs:LocationClassVocab-1.0"
+
+class AssetType(VocabString):
+    _namespace = "http://stix.mitre.org/Incident-1"
+    _binding  = incident_binding
+    _binding_class = incident_binding.AssetTypeType
+    
+    def __init__(self, value=None, count_effected=None):
+        self.count_effected = count_effected
+        super(AssetType, self).__init__(value)
+    
+    @classmethod
+    def from_obj(cls, obj, return_obj=None):
+        if not obj:
+            return None
+        if not return_obj:
+            return_obj = cls()
+        
+        super(AssetType, cls).from_obj(obj, return_obj=return_obj)
+        return_obj.count_effected = obj.get_count_effected()
+        return return_obj
+    
+    def to_obj(self, return_obj=None):
+        if not return_obj:
+            return_obj = self._binding_class()
+        
+        super(AssetType, self).to_obj(return_obj=return_obj)
+        return_obj.set_count_effected(self.count_effected)
+        return return_obj
+    
+    @classmethod
+    def from_dict(cls, d, return_obj=None):
+        if not d:
+            return None
+        if not return_obj:
+            return_obj = cls()
+            
+        super(AssetType, cls).from_dict(d, return_obj=return_obj)
+        return_obj.count_effected = d.get('count_effected')
+        return return_obj
+    
+    def to_dict(self):
+        d = super(AssetType, self).to_dict()
+        if self.count_effected:
+            d['count_effected'] = self.count_effected
+        return d
+        
+        
---
+++ stix/incident/property_affected.py
@@ -0,0 +1,203 @@
+# Copyright (c) 2014, The MITRE Corporation. All rights reserved.
+# See LICENSE.txt for complete terms.
+
+
+import stix
+from stix.common import VocabString, StructuredText
+import stix.bindings.incident as incident_binding
+
+class LossProperty(VocabString):
+    _namespace = "http://stix.mitre.org/default_vocabularies-1"
+    _XSI_TYPE = "stixVocabs:LossPropertyVocab-1.0"
+
+class AvailabilityLoss(VocabString):
+    _namespace = "http://stix.mitre.org/default_vocabularies-1"
+    _XSI_TYPE = "stixVocabs:LossPropertyVocab-1.0"
+
+
+class LossDuration(VocabString):
+    _namespace = "http://stix.mitre.org/default_vocabularies-1"
+    _XSI_TYPE = "stixVocabs:LossDurationVocab-1.0"
+
+class NonPublicDataCompromised(VocabString):
+    _namespace = "http://stix.mitre.org/Incident-1"
+    _binding  = incident_binding
+    _binding_class = incident_binding.NonPublicDataCompromisedType
+    
+    def __init__(self, value=None, data_encrypted=None):
+        self.data_encrypted = data_encrypted
+        super(NonPublicDataCompromised, self).__init__(value)
+    
+    @classmethod
+    def from_obj(cls, obj, return_obj=None):
+        if not obj:
+            return None
+        if not return_obj:
+            return_obj = cls()
+        
+        super(NonPublicDataCompromised, cls).from_obj(obj, return_obj=return_obj)
+        return_obj.data_encrypted = obj.get_data_encrypted()
+        return return_obj
+    
+    def to_obj(self, return_obj=None):
+        if not return_obj:
+            return_obj = self._binding_class()
+        
+        super(NonPublicDataCompromised, self).to_obj(return_obj=return_obj)
+        return_obj.set_data_encrypted(self.data_encrypted)
+        return return_obj
+    
+    @classmethod
+    def from_dict(cls, d, return_obj=None):
+        if not d:
+            return None
+        if not return_obj:
+            return_obj = cls()
+            
+        super(NonPublicDataCompromised, cls).from_dict(d, return_obj=return_obj)
+        return_obj.data_encrypted = d.get('data_encrypted')
+        return return_obj
+    
+    def to_dict(self):
+        d = super(NonPublicDataCompromised, self).to_dict()
+        if self.data_encrypted:
+            d['data_encrypted'] = self.data_encrypted
+        return d
+
+class PropertyAffected(stix.Entity):
+    _namespace = "http://stix.mitre.org/Incident-1"
+    _binding = incident_binding
+    _binding_class = incident_binding.PropertyAffectedType
+    
+    def __init__(self):
+        self.property = None
+        self.description_of_effect = None
+        self.type_of_availability_loss = None
+        self.duration_of_availability_loss = None
+        self.non_public_data_compromised = None
+        
+    @property
+    def property(self):
+        return self._property
+    
+    @property.setter
+    def property(self, value):
+        if not value:
+            self._property = None
+        elif isinstance(value, LossProperty):
+            self._property = value
+        else:
+            self._property = LossProperty(value)
+        
+    @property
+    def description_of_effect(self):
+        return self._description_of_effect
+    
+    @description_of_effect.setter
+    def description_of_effect(self, value):
+        if not value:
+            self._description_of_effect = None
+        elif isinstance(value, StructuredText):
+            self._description_of_effect = value
+        else:
+            self._description_of_effect = StructuredText(value)
+
+    @property
+    def type_of_availability_loss(self):
+        return self._type_of_availablity_loss
+    
+    @type_of_availability_loss.setter
+    def type_of_availability_loss(self, value):
+        if not value:
+            self._type_of_availability_loss = None
+        elif isinstance(value, AvailabilityLoss):
+            self._type_of_availability_loss = value
+        else:
+            self._type_of_availability_loss = AvailabilityLoss(value)
+            
+    @property
+    def duration_of_availability_loss(self):
+        return self._duration_of_availability_loss
+    
+    @duration_of_availability_loss.setter
+    def duration_of_availability_loss(self, value):
+        if not value:
+            self._duration_of_availability_loss = None
+        elif isinstance(value, LossDuration):
+            self._duration_of_availability_loss = value
+        else:
+            self._duration_of_availability_loss = LossDuration(value)
+    
+    @property
+    def non_public_data_compromised(self):
+        return self._non_public_data_compromised
+    
+    @non_public_data_compromised.setter
+    def non_public_data_compromised(self, value):
+        if not value:
+            self._non_public_data_compromised = None
+        elif isinstance(value, NonPublicDataCompromised):
+            self._non_public_data_compromised = value
+        else:
+            self._non_public_data_compromised = NonPublicDataCompromised(value)
+    
+    @classmethod
+    def from_obj(cls, obj, return_obj=None):
+        if not obj:
+            return None
+        if not return_obj:
+            return_obj = cls()
+            
+        return_obj.property = LossProperty.from_obj(obj.get_Property())
+        return_obj.description_of_effect = StructuredText.from_obj(obj.get_Description_Of_Effect())
+        return_obj.type_of_availability_loss = AvailabilityLoss.from_obj(obj.get_Type_Of_Availability_Loss())
+        return_obj.duration_of_availability_loss = LossDuration.from_obj(obj.get_Duration_Of_Availability_Loss())
+        return_obj.non_public_data_compromised = NonPublicDataCompromised.from_obj(obj.get_Non_Public_Data_Compromised())
+        return return_obj
+    
+    def to_obj(self, return_obj=None):
+        if not return_obj:
+            return_obj = self._binding_class()
+            
+        if self.property:
+            return_obj.set_Property(self.property.to_obj())
+        if self.description_of_effect:
+            return_obj.set_Description_Of_Effect(self.description_of_effect.to_obj())
+        if self.type_of_availability_loss:
+            return_obj.set_Type_Of_Availability_Loss(self.type_of_availability_loss.to_obj())
+        if self.duration_of_availability_loss:
+            return_obj.set_Duration_Of_Availability_Loss(self.duration_of_availability_loss.to_obj())
+        if self.non_public_data_compromised:
+            return_obj.set_Non_Public_Data_Compromised(self.non_public_data_compromised.to_obj())
+        
+        return return_obj
+    
+    @classmethod
+    def from_dict(cls, d, return_obj=None):
+        if not d:
+            return None
+        if not return_obj:
+            return_obj = cls()
+            
+        return_obj.property = LossProperty.from_dict(d.get(('property')))
+        return_obj.description_of_effect = StructuredText.from_dict(d.get('description_of_effect'))
+        return_obj.type_of_availability_loss = AvailabilityLoss.from_dict(d.get('type_of_availability_loss'))
+        return_obj.duration_of_availability_loss = LossDuration.from_dict(d.get('duration_of_availability_loss'))
+        return_obj.non_public_data_compromised = NonPublicDataCompromised.from_dict(d.get('non_public_data_compromised'))
+        
+        return return_obj
+    
+    def to_dict(self):
+        d = {}
+        if self.property:
+            d['property'] = self.property.to_dict()
+        if self.description_of_effect:
+            d['description_of_effect'] = self.description_of_effect.to_dict()
+        if self.type_of_availability_loss:
+            d['type_of_availability_loss'] = self.type_of_availability_loss.to_dict()
+        if self.duration_of_availability_loss:
+            d['duration_of_availability_loss'] = self.duration_of_availability_loss.to_dict()
+        if self.non_public_data_compromised:
+            d['non_public_data_compromised'] = self.non_public_data_compromised.to_dict()
+        return d
+    
