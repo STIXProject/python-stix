24ee307b0d745eb0533e10d894b76b3c0bdaf24a
Bryan Worrell <bworrell@mitre.org>
2014-05-05 15:03:05 -0400

    Bumped version to v1.1.0.5

--- stix/__init__.py
+++ stix/__init__.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2014, The MITRE Corporation. All rights reserved.
 # See LICENSE.txt for complete terms.
 
-__version__ = "1.1.0.4"
+__version__ = "1.1.0.5"
 
 from .base import Entity, EntityList
 
