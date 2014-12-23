312628315c73ca71a62a13b2340bec5c2ac540a9
Bryan Worrell <bworrell@mitre.org>
2014-12-23 10:49:47 -0500

    Updated RTD version table for v1.1.1.3. Bumped version to 1.1.1.3

--- stix/__init__.py
+++ stix/__init__.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2014, The MITRE Corporation. All rights reserved.
 # See LICENSE.txt for complete terms.
 
-__version__ = "1.1.1.2"
+__version__ = "1.1.1.3"
 
 from .base import Entity, EntityList
 
