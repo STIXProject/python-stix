67b58f9210f4bad7423174b6d16345dd70d6db5e
Bryan Worrell <bworrell@mitre.org>
2015-03-07 18:07:17 -0500

    Updated stix package tests

--- stix/test/core/stix_package_test.py
+++ stix/test/core/stix_package_test.py
@@ -3,195 +3,94 @@
 
 import unittest
 
-from stix.test import EntityTestCase
+from stix.test import (
+    EntityTestCase, campaign_test, coa_test, exploit_target_test,
+    incident_test, indicator_test, threat_actor_test, ttp_test
+)
+from stix.test.common import kill_chains_test
 
-from stix.core import STIXPackage
+from . import stix_header_test
+
+from stix import core
+from stix.core import stix_package
+
+
+class CampaignsTests(EntityTestCase, unittest.TestCase):
+    klass = stix_package.Campaigns
+
+    _full_dict = [
+        campaign_test.CampaignTest._full_dict
+    ]
+
+
+class COAsTests(EntityTestCase, unittest.TestCase):
+    klass = stix_package.CoursesOfAction
+
+    _full_dict = [
+        coa_test.COATests._full_dict
+    ]
+
+class ExploitTargetsTests(EntityTestCase, unittest.TestCase):
+    klass = stix_package.ExploitTargets
+
+    _full_dict = [
+        exploit_target_test.ExploitTargetTests._full_dict
+    ]
+
+class IncidentsTests(EntityTestCase, unittest.TestCase):
+    klass = stix_package.Incidents
+
+    _full_dict = [
+        incident_test.IncidentTest._full_dict
+    ]
+
+class IndicatorsTests(EntityTestCase, unittest.TestCase):
+    klass = stix_package.Indicators
+
+    _full_dict = [
+        indicator_test.IndicatorTest._full_dict
+    ]
+
+
+class ThreatActorsTests(EntityTestCase, unittest.TestCase):
+    klass = stix_package.ThreatActors
+
+    _full_dict = [
+        threat_actor_test.ThreatActorTests._full_dict
+    ]
+
+class TTPsTests(EntityTestCase, unittest.TestCase):
+    klass = stix_package.TTPs
+
+    _full_dict = {
+        'kill_chains': kill_chains_test.KillChainsTests._full_dict,
+        'ttps': [
+            ttp_test.TTPTests._full_dict
+        ]
+    }
 
 
 class STIXPackageTests(EntityTestCase, unittest.TestCase):
-    klass = STIXPackage
+    klass = core.STIXPackage
     _full_dict = {
-        'stix_header': {
-            'title': "A Title",
-            'description': "A really, really long description",
-        },
-        'ttps': {
-            'ttps': [
-            {
-                'title': "Foo",
-                'version': "1.1.1"
-            },
-            ]
-        },
-        'exploit_targets': [
-        {
-            'id': 'example:ExploitTarget-1',
-            'version': '1.1.1',
-            'title': "ExploitTarget1",
-            'description': "This is a long description about an ExploitTarget",
-            'short_description': "an ExploitTarget",
-            'information_source': {
-                'description': "Mr. Evil's enemy",
-                'identity': {
-                    'name': "Ms. Good",
-                },
-            },
-            'handling': [
-            {
-                'marking_structures': [{
-                    'marking_model_name': 'TLP',
-                    'color': "RED",
-                    'xsi:type': "tlpMarking:TLPMarkingStructureType",
-                }]
-            }
-            ]
-        }
-        ],
-        'campaigns': [
-            {
-                    'id': "example:Campaign-341",
-                    'timestamp': "2014-01-31T06:14:46",
-                    'version': '1.1.1',
-                    'title': 'Purple Elephant',
-                    'description': 'A pretty novice set of actors.',
-                    'short_description': 'novices',
-                    'names': {
-                        'names': ["Dancing Hippos", "Crazy Squirrels"],
-                    },
-                    'intended_effects': [
-                        {
-                            'timestamp': "2014-03-11T06:24:26",
-                            'value': "Doing bad stuff",
-                        },
-                        {
-                            'timestamp': "2014-03-21T06:24:26",
-                            'value': "Doing really bad stuff",
-                        }
-                    ],
-                    'status': "Ongoing",
-                    'related_ttps': {
-                        'scope': "exclusive",
-                        'ttps': [
-                            {
-                                'confidence': {'value': {'value': "Medium", 'xsi:type':'stixVocabs:HighMediumLowVocab-1.0'}},
-                                'ttp': {'title': "Stealth", 'version': "1.1.1"},
-                            }
-                        ]
-                    },
-                    'related_incidents': {
-                        'scope': "inclusive",
-                        'incidents': [
-                            {
-                                'confidence': {'value': {'value': "Medium", 'xsi:type':'stixVocabs:HighMediumLowVocab-1.0'}},
-                                'incident': {'idref': "example:Incident-2",
-                                             'version': "1.1.1"},
-                            }
-                        ]
-                    },
-                    'related_indicators': {
-                        'scope': "inclusive",
-                        'indicators': [
-                            {
-                                'confidence': {'value': {'value': "Medium", 'xsi:type':'stixVocabs:HighMediumLowVocab-1.0'}},
-                                'indicator': {'idref': "example:Indicator-77",
-                                              'version': '2.1.1'},
-                            }
-                        ]
-                    },
-                    'attribution': [{
-                        'scope': "inclusive",
-                        'threat_actors': [
-                            {
-                                'confidence': {'value': {'value': "Medium", 'xsi:type':'stixVocabs:HighMediumLowVocab-1.0'}},
-                                'threat_actor': {'title': "Campaign Actor #1",
-                                                 'version': "1.1.1"},
-                            },
-                            {
-                                'threat_actor': {'idref': "example:ThreatActor-111",
-                                                 'version': "1.1.1"},
-                            },
-                        ],
-                    }],
-                    'associated_campaigns': {
-                        'scope': "inclusive",
-                        'campaigns': [
-                            {
-                                'confidence': {'value': {'value': "Medium", 'xsi:type':'stixVocabs:HighMediumLowVocab-1.0'}},
-                                'information_source': {'description': "Threat Feed"},
-                                'campaign': {'title': "Baby Elephant", 'version': "1.1.1"},
-                            }
-                        ],
-                    },
-                    'confidence': {'value': {'value': "Medium", 'xsi:type':'stixVocabs:HighMediumLowVocab-1.0'}},
-                    'activity': [
-                            {
-                                'date_time': "2012-01-01T08:45:31",
-                                'description': "The first bad thing"
-                            },
-                            {
-                                'date_time': "2012-01-02T08:45:31",
-                                'description': "Another bad thing"
-                            },
-                    ],
-                    'information_source': {
-                        'description': "A former member of the campaign.",
-                        'identity': {
-                            'name': "Mr. D. Fector",
-                        },
-                    },
-                    'handling': [
-                        {
-                            'marking_structures': [{
-                                'marking_model_name': 'TLP',
-                                'color': "RED",
-                                'xsi:type': "tlpMarking:TLPMarkingStructureType",
-                            }]
-                        }
-                    ],
-                    'related_packages': {
-                        'packages': [
-                            {'idref': "example:Package-AB", 'relationship': "Parent"},
-                            {'idref': "example:Package-CD", 'relationship': "Child"}
-                        ]
-                    }
+        'stix_header': stix_header_test.STIXHeaderTests._full_dict,
+        'campaigns': CampaignsTests._full_dict,
+        'courses_of_action': COAsTests._full_dict,
+        'exploit_targets': ExploitTargetsTests._full_dict,
+        'incidents': IncidentsTests._full_dict,
+        'indicators': IndicatorsTests._full_dict,
+        'observables':  {
+            'major_version': 2,
+            'minor_version': 1,
+            'update_version': 0,
+            'observables': [
+                {
+                    'idref': "example:Observable-1"
                 }
-        ],
-        'courses_of_action': [
-        {
-            'id': 'example:coa-1',
-            'version': '1.1.1',
-            'timestamp': "2014-03-20T04:35:12",
-            'title': "COA1",
-            'description': "This is a long description about a course of action",
-            'short_description': "a COA",
-            'stage': {"value" : "Remedy", "xsi:type" : "stixVocabs:COAStageVocab-1.0"},
-            'type': {'value': 'Redirection','xsi:type': 'stixVocabs:CourseOfActionTypeVocab-1.0'},
-            'cost': {
-                'value': "100"
-            },
-            'efficacy': {
-                'value': "Half"
-            },
-            'impact': {
-                'value': "Large"
-            },
-            'information_source': {
-                'description': "Mr. Evil's enemy",
-                'identity': {
-                    'name': "Ms. Good",
-                },
-            },
-            'handling': [
-            {
-                'marking_structures': [{
-                    'marking_model_name': 'TLP',
-                    'color': "GREEN",
-                    'xsi:type': "tlpMarking:TLPMarkingStructureType",
-                }]
-            }
             ]
-        }
-        ],
+        },
+        'threat_actors': ThreatActorsTests._full_dict,
+        'ttps': TTPsTests._full_dict,
         'version': "1.1.1"
     }
 
