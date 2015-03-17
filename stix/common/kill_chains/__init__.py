8d12d3e165fae1bc2dd90b65cc049d5955c2b5aa
Bryan Worrell <bworrell@mitre.org>
2015-03-17 14:57:38 -0400

    Moved lmco specific kill chain code into an lmco module. Changed kill_chains.py module into kill_chains subpackage.

--- stix/common/kill_chains/__init__.py
+++ stix/common/kill_chains/__init__.py
@@ -10,7 +10,7 @@ class KillChain(stix.Entity):
     _binding = common_binding
     _namespace = 'http://stix.mitre.org/common-1'
     _binding_class = _binding.KillChainType
-    
+
     def __init__(self, id_=None, name=None, definer=None, reference=None):
         self.id_ = id_
         self.name = name
@@ -18,31 +18,31 @@ class KillChain(stix.Entity):
         self.reference = reference
         self.number_of_phases = None # can we just do len(self.kill_chain_phases)?
         self.kill_chain_phases = None
-    
+
     @property
     def kill_chain_phases(self):
         return self._kill_chain_phases
-    
+
     @kill_chain_phases.setter
     def kill_chain_phases(self, value):
         self._kill_chain_phases = _KillChainPhases(value)
-    
+
     def add_kill_chain_phase(self, value):
         self.kill_chain_phases.append(value)
-    
+
     def to_obj(self, return_obj=None, ns_info=None):
         super(KillChain, self).to_obj(return_obj=return_obj, ns_info=ns_info)
 
         if not return_obj:
             return_obj = self._binding_class()
-    
+
         return_obj.id = self.id_
         return_obj.name = self.name
         return_obj.definer = self.definer
         return_obj.reference = self.reference
         return_obj.number_of_phases = self.number_of_phases
         return_obj.Kill_Chain_Phase = self.kill_chain_phases.to_obj(ns_info=ns_info)
-    
+
         return return_obj
 
     def __eq__(self, other):
@@ -63,16 +63,16 @@ class KillChain(stix.Entity):
             return None
         if not return_obj:
             return_obj = cls()
-    
+
         return_obj.id_ = obj.id
         return_obj.name = obj.name
         return_obj.definer = obj.definer
         return_obj.reference = obj.reference
         return_obj.number_of_phases = obj.number_of_phases
         return_obj.kill_chain_phases = _KillChainPhases.from_obj(obj.Kill_Chain_Phase)
-    
+
         return return_obj
-    
+
     @classmethod
     def from_dict(cls, d, return_obj=None):
         if not d:
@@ -88,7 +88,7 @@ class KillChain(stix.Entity):
         return_obj.number_of_phases = get('number_of_phases')
         return_obj.kill_chain_phases = \
             _KillChainPhases.from_dict(get('kill_chain_phases'))
-    
+
         return return_obj
 
 
@@ -99,13 +99,13 @@ class KillChains(stix.EntityList):
     _contained_type = KillChain
     _binding_var = "Kill_Chain"
     _inner_name = "kill_chains"
-    
+
 
 class KillChainPhase(stix.Entity):
     _binding = common_binding
     _namespace = 'http://stix.mitre.org/common-1'
     _binding_class = _binding.KillChainPhaseType
-    
+
     def __init__(self, phase_id=None, name=None, ordinality=None):
         self.phase_id = phase_id
         self.name = name
@@ -122,14 +122,14 @@ class KillChainPhase(stix.Entity):
     @property
     def ordinality(self):
         return self._ordinality
-    
+
     @ordinality.setter
     def ordinality(self, value):
         if value is not None:
             self._ordinality = int(value)
         else:
             self._ordinality = None
-    
+
     def to_obj(self, return_obj=None, ns_info=None):
         if not return_obj:
             return_obj = self._binding_class()
@@ -139,7 +139,7 @@ class KillChainPhase(stix.Entity):
         return_obj.phase_id = self.phase_id
         return_obj.name = self.name
         return_obj.ordinality = self.ordinality
-    
+
         return return_obj
 
     def __eq__(self, other):
@@ -165,24 +165,24 @@ class KillChainPhase(stix.Entity):
 
         if not return_obj:
             return_obj = cls()
-    
+
         return_obj.phase_id = obj.phase_id
         return_obj.name = obj.name
         return_obj.ordinality = obj.ordinality
-        
+
         return return_obj
-    
+
     @classmethod
     def from_dict(cls, d, return_obj=None):
         if not d:
             return None
         if not return_obj:
             return_obj = cls()
-    
+
         return_obj.phase_id = d.get('phase_id')
         return_obj.name = d.get('name')
         return_obj.ordinality = d.get('ordinality')
-        
+
         return return_obj
 
     def to_dict(self):
@@ -202,35 +202,35 @@ class KillChainPhaseReference(KillChainPhase):
     def to_obj(self, return_obj=None, ns_info=None):
         if not return_obj:
             return_obj = self._binding_class()
-    
+
         super(KillChainPhaseReference, self).to_obj(return_obj=return_obj, ns_info=ns_info)
         return_obj.kill_chain_id = self.kill_chain_id
         return_obj.kill_chain_name = self.kill_chain_name
         return return_obj
-    
+
     @classmethod
     def from_obj(cls, obj, return_obj=None):
         if not obj:
             return None
         if not return_obj:
             return_obj = cls()
-    
+
         super(KillChainPhaseReference, cls).from_obj(obj, return_obj=return_obj)
 
         return_obj.kill_chain_id = obj.kill_chain_id
         return_obj.kill_chain_name = obj.kill_chain_name
         return return_obj
-    
+
     def to_dict(self):
         return super(KillChainPhaseReference, self).to_dict()
-    
+
     @classmethod
     def from_dict(cls, d, return_obj=None):
         if not d:
             return None
         if not return_obj:
             return_obj = cls()
-    
+
         super(KillChainPhaseReference, cls).from_dict(d, return_obj=return_obj)
         return_obj.kill_chain_id = d.get('kill_chain_id')
         return_obj.kill_chain_name = d.get('kill_chain_name')
@@ -245,15 +245,10 @@ class KillChainPhasesReference(stix.EntityList):
     _binding_var = "Kill_Chain_Phase"
     _inner_name = "kill_chain_phases"
 
-    def _is_lmco(self, value):
-        return any(value.phase_id == x.phase_id for x in LMCO_KILL_CHAIN_PHASES)
 
     def _fix_value(self, value):
-        if isinstance(value, KillChainPhase) and self._is_lmco(value):
-            return KillChainPhaseReference(
-                phase_id=value.phase_id,
-                kill_chain_id=LMCO_KILL_CHAIN.id_
-            )
+        if isinstance(value, KillChainPhase):
+            return KillChainPhaseReference(phase_id=value.phase_id)
 
         return super(KillChainPhasesReference, self)._fix_value(value)
 
@@ -261,86 +256,3 @@ class KillChainPhasesReference(stix.EntityList):
 # NOT AN ACTUAL STIX TYPE!
 class _KillChainPhases(stix.TypedList):
     _contained_type = KillChainPhase
-
-
-LMCO_KILL_CHAIN = None
-LMCO_KILL_CHAIN_PHASES = None
-PHASE_RECONNAISSANCE = None
-PHASE_WEAPONIZE = None
-PHASE_DELIVERY = None
-PHASE_EXPLOITATION = None
-PHASE_INSTALLATION = None
-PHASE_COMMAND_AND_CONTROL = None
-PHASE_ACTIONS_AND_OBJECTIVES = None
-
-
-def __create_lmco():
-    global LMCO_KILL_CHAIN, LMCO_KILL_CHAIN_PHASES
-    global PHASE_RECONNAISSANCE, PHASE_WEAPONIZE, PHASE_DELIVERY
-    global PHASE_EXPLOITATION, PHASE_INSTALLATION, PHASE_COMMAND_AND_CONTROL
-    global PHASE_ACTIONS_AND_OBJECTIVES
-
-    if LMCO_KILL_CHAIN:
-        return
-
-    PHASE_RECONNAISSANCE = KillChainPhase(
-        phase_id="stix:TTP-af1016d6-a744-4ed7-ac91-00fe2272185a",
-        name="Reconnaissance",
-        ordinality="1"
-    )
-
-    PHASE_WEAPONIZE = KillChainPhase(
-        phase_id="stix:TTP-445b4827-3cca-42bd-8421-f2e947133c16",
-        name="Weaponization",
-        ordinality="2"
-    )
-
-    PHASE_DELIVERY  = KillChainPhase(
-        phase_id="stix:TTP-79a0e041-9d5f-49bb-ada4-8322622b162d",
-        name="Delivery",
-        ordinality="3"
-    )
-
-    PHASE_EXPLOITATION = KillChainPhase(
-        phase_id="stix:TTP-f706e4e7-53d8-44ef-967f-81535c9db7d0",
-        name="Exploitation",
-        ordinality="4"
-    )
-
-    PHASE_INSTALLATION  = KillChainPhase(
-        phase_id="stix:TTP-e1e4e3f7-be3b-4b39-b80a-a593cfd99a4f",
-        name="Installation",
-        ordinality="5"
-    )
-
-    PHASE_COMMAND_AND_CONTROL = KillChainPhase(
-        phase_id="stix:TTP-d6dc32b9-2538-4951-8733-3cb9ef1daae2",
-        name="Command and Control",
-        ordinality="6"
-
-    )
-
-    PHASE_ACTIONS_AND_OBJECTIVES  = KillChainPhase(
-        phase_id="stix:TTP-786ca8f9-2d9a-4213-b38e-399af4a2e5d6",
-        name="Actions on Objectives",
-        ordinality="7"
-    )
-
-    LMCO_KILL_CHAIN_PHASES = (
-        PHASE_RECONNAISSANCE, PHASE_WEAPONIZE, PHASE_DELIVERY,
-        PHASE_EXPLOITATION, PHASE_INSTALLATION, PHASE_COMMAND_AND_CONTROL,
-        PHASE_ACTIONS_AND_OBJECTIVES
-    )
-
-    LMCO_KILL_CHAIN = KillChain(
-        id_="stix:TTP-af3e707f-2fb9-49e5-8c37-14026ca0a5ff",
-        name="LM Cyber Kill Chain",
-        definer="LMCO",
-        reference="http://www.lockheedmartin.com/content/dam/lockheed/data/corporate/documents/LM-White-Paper-Intel-Driven-Defense.pdf"
-    )
-
-    LMCO_KILL_CHAIN.kill_chain_phases.extend(LMCO_KILL_CHAIN_PHASES)
-
-
-# Set the module-level LMCO Kill Chain object
-__create_lmco()
