3b4aa2f07374f5cda0b9f0da153ce48b3deb756e
Greg Back <gback@mitre.org>
2015-04-01 10:39:04 -0400

    Bump version to 1.1.1.5-dev

--- stix/__init__.py
+++ stix/__init__.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2015, The MITRE Corporation. All rights reserved.
 # See LICENSE.txt for complete terms.
 
-__version__ = "1.1.1.4"
+__version__ = "1.1.1.5-dev"
 
 
 def supported_stix_version():
