3536a83ce221bc216d3a330171236b7b821b8844
Greg Back <gback@mitre.org>
2015-04-03 10:36:36 -0400

    Clean up and standardize docs and conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -16,9 +16,9 @@ extensions = [
     'sphinxcontrib.napoleon',
 ]
 
-napoleon_use_admonition_for_examples = False
-
-intersphinx_mapping = {'http://docs.python.org/': None}
+intersphinx_mapping = {
+    'python': ('http://docs.python.org/', None),
+}
 
 templates_path = ['_templates']
 source_suffix = '.rst'
@@ -28,23 +28,22 @@ rst_prolog = """
 **Version**: {0}
 """.format(release)
 
-exclude_patterns = ['_build', '_includes', 'api_vs_bindings/*_snippet.rst']
-
-pygments_style = 'sphinx'
+exclude_patterns = [
+    '_build',
+    '_includes',
+    'api_vs_bindings/*_snippet.rst',
+]
 
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-if not on_rtd:  # only import and set the theme if we're building docs locally
+if not on_rtd:
     import sphinx_rtd_theme
     html_theme = 'sphinx_rtd_theme'
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 else:
     html_theme = 'default'
 
-html_sidebars = {"**": ['localtoc.html', 'relations.html', 'sourcelink.html',
-'searchbox.html', 'links.html']}
-
 latex_elements = {}
 latex_documents = [
-  ('index', 'python-stix.tex', u'python-stix Documentation',
-   u'The MITRE Corporation', 'manual'),
+    ('index', 'python-stix.tex', u'python-stix Documentation',
+     u'The MITRE Corporation', 'manual'),
 ]
