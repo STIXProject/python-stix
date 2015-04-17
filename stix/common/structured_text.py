6779e1a82fdd37a64d90e44c097535a9867c865e
Bryan Worrell <bworrell@mitre.org>
2015-04-17 16:12:10 -0400

    Began work on multi-description support.

--- stix/common/structured_text.py
+++ stix/common/structured_text.py
@@ -7,28 +7,43 @@ import stix.bindings.stix_common as stix_common_binding
 
 class StructuredText(stix.Entity):
     _binding = stix_common_binding
+    _binding_class = _binding.StructuredTextType
     _namespace = 'http://stix.mitre.org/common-1'
 
     def __init__(self, value=None):
+        self.id_ = None
         self.value = value
         self.structuring_format = None
+        self.ordinality = None
 
     def to_obj(self, return_obj=None, ns_info=None):
+        if not return_obj:
+            return_obj = self._binding_class()
+
         super(StructuredText, self).to_obj(
             return_obj=return_obj,
             ns_info=ns_info
         )
 
-        text_obj = self._binding.StructuredTextType()
+        return_obj.id = self.id_
+        return_obj.valueOf_ = self.value
+        return_obj.ordinality = self.ordinality
+        return_obj.structuring_format = self.structuring_format
 
-        text_obj.valueOf_ = self.value
-        if self.structuring_format is not None:
-            text_obj.structuring_format = self.structuring_format
-        return text_obj
+        return return_obj
+
+    def is_plain(self):
+        plain = (
+            (not self.id_) and
+            (not self.structuring_format) and
+            (self.ordinality in (1, None))
+        )
+
+        return plain
 
     def to_dict(self):
         # Return a plain string if there is no format specified.
-        if self.structuring_format is None:
+        if self.is_plain():
             return self.value
         else:
             return super(StructuredText, self).to_dict()
@@ -41,7 +56,9 @@ class StructuredText(stix.Entity):
         if not return_obj:
             return_obj = cls()
 
+        return_obj.id_ = obj.id
         return_obj.value = obj.valueOf_
+        return_obj.ordinality = obj.ordinality
         return_obj.structuring_format = obj.structuring_format
 
         return return_obj
@@ -57,7 +74,9 @@ class StructuredText(stix.Entity):
         if not isinstance(d, dict):
             return_obj.value = d
         else:
+            return_obj.id_ = d.get('id')
             return_obj.value = d.get('value')
+            return_obj.ordinality = d.get('ordinality')
             return_obj.structuring_format = d.get('structuring_format')
 
         return return_obj
@@ -67,3 +86,16 @@ class StructuredText(stix.Entity):
 
     def __unicode__(self):
         return unicode(self.value)
+
+
+class StructuredTextList(stix.TypedList):
+    _contained_type = StructuredText
+
+    def __iter__(self):
+        return sorted(self._inner, key=lambda x: x.ordinality)
+
+    def __getitem__(self, item):
+        pass
+
+    def __setitem__(self, key, value):
+        pass
