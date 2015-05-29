67adb111cdbc707a55a296132509b4f3610a5788
Greg Back <gback@mitre.org>
2015-05-29 13:58:38 -0500

    Bump sphinx version to 1.3.1 to match Read The Docs.

--- setup.py
+++ setup.py
@@ -25,11 +25,8 @@ with open('README.rst') as f:
 
 extras_require = {
     'docs': [
-        'Sphinx==1.2.1',
-        # TODO: remove when updating to Sphinx 1.3, since napoleon will be
-        # included as sphinx.ext.napoleon
-        'sphinxcontrib-napoleon==0.2.4',
-        'sphinx_rtd_theme==0.1.7',
+        'Sphinx==1.3.1',
+        'sphinx_rtd_theme==0.1.8',
     ],
     'test': [
         'nose==1.3.0',
