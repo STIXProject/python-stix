577ae8c8c08dc9b3b05180b53836edba7fb09c67
Bryan Worrell <bworrell@mitre.org>
2015-10-01 09:55:11 -0400

    Updated ToolInformation to use TypedFields.

--- stix/common/tools.py
+++ stix/common/tools.py
@@ -2,33 +2,29 @@
 # See LICENSE.txt for complete terms.
 
 # external
+from mixbox import fields
 import cybox.common
 
 # internal
 import stix
-import stix.utils as utils
 import stix.bindings.stix_common as common_binding
 
 # relative
 from .structured_text import StructuredTextList
 
+
 class ToolInformation(stix.Entity, cybox.common.ToolInformation):
     _namespace = 'http://stix.mitre.org/common-1'
     _binding = common_binding
     _binding_class = common_binding.ToolInformationType
-    
+
+    title = fields.TypedField("Title")
+    short_descriptions = fields.TypedField("Short_Description", StructuredTextList, key_name="short_description")
+
     def __init__(self, title=None, short_description=None, tool_name=None, tool_vendor=None):
         super(ToolInformation, self).__init__(tool_name=tool_name, tool_vendor=tool_vendor)
         self.title = title
         self.short_description = short_description
-    
-    @property
-    def title(self):
-        return self._title
-
-    @title.setter
-    def title(self, value):
-        self._title = value
 
     @property
     def short_description(self):
@@ -52,35 +48,6 @@ class ToolInformation(stix.Entity, cybox.common.ToolInformation):
     def short_description(self, value):
         self.short_descriptions = value
 
-    @property
-    def short_descriptions(self):
-        """A :class:`.StructuredTextList` object, containing short descriptions
-        about the purpose or intent of this object.
-
-        This is typically used for the purpose of providing multiple
-        short descriptions with different classificaton markings.
-
-        Iterating over this object will yield its contents sorted by their
-        ``ordinality`` value.
-
-        Default Value: Empty :class:`.StructuredTextList` object.
-
-        Note:
-            IF this is set to a value that is not an instance of
-            :class:`.StructuredText`, an effort will ne made to convert it.
-            If this is set to an iterable, any values contained that are not
-            an instance of :class:`.StructuredText` will be be converted.
-
-        Returns:
-            An instance of :class:`.StructuredTextList`
-
-        """
-        return self._short_description
-
-    @short_descriptions.setter
-    def short_descriptions(self, value):
-        self._short_description = StructuredTextList(value)
-
     def add_short_description(self, description):
         """Adds a description to the ``short_descriptions`` collection.
 
@@ -100,33 +67,33 @@ class ToolInformation(stix.Entity, cybox.common.ToolInformation):
             ns_info=ns_info
         )
 
-        return_obj.Title = self.title        
-        if self.short_descriptions:
-            return_obj.Short_Description = self.short_descriptions.to_obj(ns_info=ns_info)
-
         return return_obj
 
     @classmethod
     def from_obj(cls, obj, return_obj=None):
         if not obj:
             return None
+
         if not return_obj:
             return_obj = cls()
 
         cybox.common.ToolInformation.from_obj(obj, return_obj)
-        
+
         return_obj.title = obj.Title
         return_obj.short_descriptions = StructuredTextList.from_obj(obj.Short_Description)
         
         return return_obj
 
     def to_dict(self):
-        return utils.to_dict(self)
+        d = stix.Entity.to_dict(self)
+        d.update(cybox.common.ToolInformation.to_dict(self))
+        return d
 
     @classmethod
     def from_dict(cls, dict_repr, return_obj=None):
         if not dict_repr:
             return None
+
         if not return_obj:
             return_obj = cls()
 
