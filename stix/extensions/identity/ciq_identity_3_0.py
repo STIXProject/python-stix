085b2d86da3f18113ef77fdc5dd2a901fdf8dd15
Michael Chisholm <chisholm@mitre.org>
2015-10-07 13:17:52 -0400

    A better fix to the missing namespace collect bug.  I realized that the super calls to to_obj() should have done the collection, but ns_info hadn't been passed up to that method.

--- stix/extensions/identity/ciq_identity_3_0.py
+++ stix/extensions/identity/ciq_identity_3_0.py
@@ -66,11 +66,10 @@ class CIQIdentity3_0Instance(common.Identity):
         self._specification = value
 
     def to_obj(self, return_obj=None, ns_info=None):
-        self._collect_ns_info(ns_info)
         if not return_obj:
             return_obj = self._binding_class()
 
-        super(CIQIdentity3_0Instance, self).to_obj(return_obj)
+        super(CIQIdentity3_0Instance, self).to_obj(return_obj, ns_info)
 
         # return_obj.id = self.id_
         # return_obj.idref = self.idref_
