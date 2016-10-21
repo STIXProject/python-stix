1a5c2aeb30e67bfa52958296c8d314df5d742210
Greg Back <gback@mitre.org>
2016-10-21 16:20:05 -0500

    Bump version to 1.1.1.7

--- stix/version.py
+++ stix/version.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2015, The MITRE Corporation. All rights reserved.
+# Copyright (c) 2016, The MITRE Corporation. All rights reserved.
 # See LICENSE.txt for complete terms.
 
-__version__ = "1.1.1.7.dev0"
+__version__ = "1.1.1.7"
